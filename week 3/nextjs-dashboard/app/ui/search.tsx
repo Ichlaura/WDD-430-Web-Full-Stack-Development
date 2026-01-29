1b191456caf2bd00119f1666dcf85878f44e1d54
laura nunez <laupao41@gmail.com>
2026-01-30 00:33:21 +0900

    chapter 10 done

--- week 3/nextjs-dashboard/app/ui/search.tsx
+++ week 3/nextjs-dashboard/app/ui/search.tsx
@@ -12,6 +12,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
 
+    // reset page when searching
+    params.set('page', '1');
+
     if (term) {
       params.set('query', term);
     } else {
@@ -19,20 +22,22 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }, 300); // 👈 espera 300ms antes de buscar
+  }, 300);
 
   return (
     <div className="relative flex flex-1 shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+
       <input
+        id="search"
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
+
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
